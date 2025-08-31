5304d32d0e75267c9cd66564aaa78e051d1d45cb
hinako shimamori <isola16858foresta198401@gmail.com>
2025-08-31 21:26:28 +0900

    シーディングファイルの追加

--- src/database/seeders/BreakTimesTableSeeder.php
+++ src/database/seeders/BreakTimesTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Attendance;
 
 class BreakTimesTableSeeder extends Seeder
 {
@@ -13,6 +15,447 @@ class BreakTimesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('break_times')->insert([
+            [
+                'attendance_id' => 1,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 2,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 3,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 4,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 5,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 6,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 7,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 8,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 9,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 10,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 11,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 12,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 13,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 14,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 15,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 16,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 17,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 18,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 19,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 20,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 21,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 22,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 23,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 24,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 25,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 25,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 26,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 27,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 27,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 28,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 29,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 29,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 30,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 31,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 31,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 32,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 33,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 33,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 34,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 35,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 35,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 36,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 37,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 37,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 38,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 39,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 39,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 40,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 41,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 41,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 42,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 43,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 43,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 44,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 45,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 45,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 46,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 47,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 47,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 48,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 49,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 49,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 50,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 51,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 51,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 52,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 53,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 53,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 54,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 55,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 55,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 56,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 57,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 57,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 58,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 59,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 59,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 60,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 61,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 61,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 62,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 63,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 63,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 64,
+                'start_time' => '12:00',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 65,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 65,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+            [
+                'attendance_id' => 67,
+                'start_time' => '10:30',
+                'end_time' => '10:45',
+            ],
+            [
+                'attendance_id' => 67,
+                'start_time' => '12:15',
+                'end_time' => '13:00',
+            ],
+        ]);
     }
 }
